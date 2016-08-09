40cdf559c8ffdd871ee990263648b8611157074b
Olaf Lessenich <lessenic@fim.uni-passau.de>
2016-08-09 19:30:51 +0200

    Several modifications
    - Usage of python3
    - '-a', '--agressive': Do not pause while downloading.
                           Default: False.
    - '-n', '--dry-run':   Do not actually download anything.
                           Default: False.
    - '-s', '--start':     First message in range to download.
                           Default: First available message
    
    The default behavior is to pause 30 seconds every 1000 messages while
    downloading to reduce the load on the load on the gmane servers.
    
    The argument '-s' has replaced the usage of the number file. Also, the
    default behavior has changed: If no start message is specified, all
    available messages on the server will be pulled.

--- nntp2mbox.py
+++ nntp2mbox.py
@@ -1,59 +1,102 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+#
+# Distributed under terms of the MIT license.
+#
+# Copyright (c) 2009 Sven Velt
+#                    original code,
+#                    please see https://github.com/wAmpIre/nntp2mbox
+#                    for the original project by Sven
+#
+# Copyright (c) 2016 Olaf Lessenich
+#                    ported to python3, change in functionality
+#
 
+import argparse
+import email
+import mailbox
 import nntplib
 import sys
-import email
-
-outfile = file(sys.argv[-1], 'a')
-
-nntpconn = nntplib.NNTP('news.gmane.org')
-
-resp, count, first, last, name = nntpconn.group(sys.argv[-1])
-print 'Group', name, 'has', count, 'articles, range', first, 'to', last
-
-last = int(last)
-startnr = last - 500
-
-if startnr < 1:
-	startnr = 1
-
-try:
-	nofile = file(sys.argv[-1] + ".cfg", 'r')
-	startnr = int(nofile.readline())
-	nofile.close()
-except IOError:
-	print 'No number file found, starting at ' + str(startnr)
-
-if startnr < 1:
-	startnr = 1
-if startnr < last:
-	last = last +1
+import time
 
 
+def download(group, aggressive, dry_run, start=None):
+    """
+    The default behavior is to pause 30 seconds every 1000 messages while
+    downloading to reduce the load on the load on the gmane servers.
+    This can be skipped by supplying the aggressive flag.
+    """
+
+    mbox = mailbox.mbox(group + '.mbox')
+    mbox.lock()
+
+    nntpconn = nntplib.NNTP('news.gmane.org')
+
+    resp, count, first, last, name = nntpconn.group(group)
+    print(
+        'Group %s has %d articles, range %d to %d' %
+        (name, count, first, last))
+
+    last = int(last)
+
+    if start:
+        startnr = start
+
+        if startnr < first:
+            startnr = first
+
+        if startnr > last:
+            startnr = last
+
+    else:
+        startnr = first
+
+    if not start:
+        print('No start message provided, starting at %d' % startnr)
+
+    for msgno in range(startnr, last):
+        try:
+            if not aggressive and (msgno % 1000 == 0) and (msgno != startnr):
+                print('%d: Sleep 30 seconds' % msgno)
+                time.sleep(30)
+
+            if dry_run:
+                print('Dry-run: download message no. %d' % msgno)
+                pass
+
+            resp, info = nntpconn.article(str(msgno))
+
+            text = str()
+            for line in info.lines:
+                text += line.decode(encoding='UTF-8') + "\n"
+
+            msg = email.message_from_string(text)
+            mbox.add(msg)
+
+            print('%d(%s): %s' % (info.number, msgno, info.message_id))
+        except:
+            print(sys.exc_info()[0])
+            pass
+
+    mbox.flush()
+    mbox.unlock()
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a",
+                        "--aggressive",
+                        help="Disable waiting during a download",
+                        action="store_true")
+    parser.add_argument("-n",
+                        "--dry-run",
+                        help="perform a trial run with no changes made",
+                        action="store_true")
+    parser.add_argument("-s",
+                        "--start",
+                        help="First message in range",
+                        type=int)
+    parser.add_argument("groups", default="[]", nargs="+")
+    args = parser.parse_args()
 
-for msgno in range( startnr, last ):
-	try:
-		resp, number, id, list = nntpconn.article(str(msgno))
-
-		text = str()
-		for line in list:
-			text += line + "\n"
-
-		msg = email.message_from_string(text)
-		outfile.write(str(msg))
-		outfile.write('\n')
-
-		print '%s(%s): %s' % (number, msgno, id)
-	except:
-		pass
-
-outfile.close()
-
-
-nofile = file(sys.argv[-1] + ".cfg", 'w')
-nofile.write(str(last))
-nofile.close()
-
-
+    for group in args.groups:
+        download(group, args.aggressive, args.dry_run, args.start)
